012010dbca3b107b5944a87ad8c94a270d800d81
Stanislav Katkov <skatkov@cheddar.me>
2023-11-27 10:59:13 +0100

    small tweaks

--- lib/yard-markdown.rb
+++ lib/yard-markdown.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-dir = File.dirname(__FILE__)
-$LOAD_PATH.unshift dir unless $LOAD_PATH.include?(dir)
-
 require "yard"
 require_relative "markdown/version"
 
@@ -12,4 +9,4 @@ module YARD
 end
 
 puts "YARD::Markdown loaded"
-YARD::Templates::Engine.register_template_path dir + '/../templates'
+YARD::Templates::Engine.register_template_path File.dirname(__FILE__) + '/../templates'
