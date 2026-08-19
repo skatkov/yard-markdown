e79efa789357b808bef0a299cacddcca1d90ce5b
Stanislav (Stas) Katkov <git@skatkov.com>
2026-08-19 11:07:39 +0200

    inline tag formatting helpers

--- lib/yard/markdown/tag_formatting_helper.rb
+++ lib/yard/markdown/tag_formatting_helper.rb
@@ -10,8 +10,12 @@ module YARD
       # @return [String] Markdown representation of the object's tags.
       def self.render_tags(object)
         example_tags, regular_tags = object.tags.partition { |tag| tag.tag_name == "example" }
-        regular = render_regular_tags(regular_tags)
-        examples = render_example_tags(example_tags)
+        regular = regular_tags.map { |tag| "- #{format_tag(tag)}" }.join("\n")
+        examples = example_tags.map do |tag|
+          name = tag.name.to_s.rstrip
+          title = name.empty? ? "**@example**" : "**@example #{name}**"
+          [title, "```ruby", tag.text.to_s.rstrip, "```"].join("\n")
+        end
         [regular, examples].reject(&:empty?).join("\n\n")
       end
 
@@ -22,57 +26,14 @@ module YARD
       def self.format_tag(tag)
         name = tag.name.to_s
         text = tag.text.to_s
+        types = Array(tag.types).map { |type| type.to_s.strip }.reject(&:empty?)
         [
           "**@#{tag.tag_name}**",
           ("`#{name}`" unless name.lstrip.empty?),
-          formatted_tag_types(tag.types),
+          ("[#{types.join(", ")}]" unless types.empty?),
           (text.strip unless text.lstrip.empty?)
         ].compact.join(" ")
       end
-
-      # Normalizes tag type declarations into printable strings.
-      #
-      # @param types [Array<Object>, nil] Raw tag types from YARD.
-      # @return [Array<String>] Cleaned type strings.
-      def self.normalized_tag_types(types)
-        Array(types).map(&:to_s).map(&:strip).reject(&:empty?)
-      end
-
-      # Formats normalized types as a tag section.
-      #
-      # @param types [Array<Object>, nil] Raw tag types.
-      # @return [String, nil] Formatted types, if present.
-      def self.formatted_tag_types(types)
-        cleaned_types = normalized_tag_types(types)
-        "[#{cleaned_types.join(", ")}]" unless cleaned_types.empty?
-      end
-
-      # Formats one example tag as a fenced Ruby block.
-      #
-      # @param tag [YARD::Tags::Tag] Example tag.
-      # @return [String] Example Markdown.
-      def self.format_example_tag(tag)
-        name = tag.name.to_s.rstrip
-        title = name.empty? ? "**@example**" : "**@example #{name}**"
-        [title, "```ruby", tag.text.to_s.rstrip, "```"].join("\n")
-      end
-
-      # Formats regular tags as list items.
-      #
-      # @param tags [Array<YARD::Tags::Tag>] Non-example tags.
-      # @return [String] Tag list Markdown.
-      def self.render_regular_tags(tags)
-        tags.map { |tag| "- #{format_tag(tag)}" }.join("\n")
-      end
-
-      # Formats example tags as separated fenced blocks.
-      #
-      # @param tags [Array<YARD::Tags::Tag>] Example tags.
-      # @return [Array<String>] Formatted examples.
-      def self.render_example_tags(tags)
-        tags.map { |tag| format_example_tag(tag) }
-      end
-      private_class_method :formatted_tag_types, :format_example_tag, :render_regular_tags, :render_example_tags
     end
   end
 end
