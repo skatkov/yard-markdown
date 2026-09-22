3df2ad3a9f359ab9b1167b635b8f05f921e4dfa3
Stanislav (Stas) Katkov <git@skatkov.com>
2026-09-22 15:03:10 +0200

    rebuild incremental index from full registry

--- templates/default/fulldoc/markdown/setup.rb
+++ templates/default/fulldoc/markdown/setup.rb
@@ -10,6 +10,7 @@ include YARD::Markdown::ObjectListingHelper,
 # @return [void]
 def init
   objects = run_verifier(options.objects).reject { |item| item.name == :root }
+  index_objects = run_verifier(YARD::Registry.all(:module, :class))
   files = Array(options.files).select { |file| file.filename.match?(YARD::Markdown::FILE_PATTERN) }
   options.copied_file_aliases = files.to_h do |file|
     path = Pathname.new(file.filename).cleanpath.to_s
@@ -29,7 +30,7 @@ def init
     log.backtrace(e)
   end
 
-  serialize_index(objects, files)
+  serialize_index(index_objects, files)
 end
 
 # Renders the markdown template for a single namespace object.
